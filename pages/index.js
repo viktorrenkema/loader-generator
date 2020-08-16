66b7818f3b9673371eed052c7590dbf18dafaa20
viktorrenkema <vrenkema@gmail.com>
2020-08-16 23:00:05 +0200

    Stuck with yellow h1 bug

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Head from "next/head";
-import TwitterData1 from "../components/dataset1";
-import TwitterData2 from "../components/dataset2";
-import TwitterData3 from "../components/dataset3";
-import Blob from "../components/blob";
-import Polygon from "../components/polygon";
-import Dots, { Dot } from "../components/dots";
-import ExpDots from "../components/exp-dots";
+import { motion, AnimateSharedLayout } from "framer-motion";
 import Form from "../components/form";
-import { GraphOne } from "../components/dataset1";
-import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+
+import MoreInfo from "../components/moreinfo";
 
 export default function Home(props) {
   const [tab, setCurrentTab] = React.useState(1);
@@ -17,19 +11,17 @@ export default function Home(props) {
   return (
     <div className="container">
       <Head>
-        <title>Data Visualization</title>
+        <title>Loading component generator</title>
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="title">
-          Data Visualization<Dot></Dot>
-        </h1>
+        <h1 className="title">Loading component generator</h1>
 
         <ul>
           <AnimateSharedLayout transition={{ duration: 0.5 }}>
             <motion.li onClick={() => setCurrentTab(1)}>
-              Dots
+              Generator
               {tab === 1 && (
                 <motion.div
                   layoutId="underline"
@@ -43,7 +35,7 @@ export default function Home(props) {
               )}
             </motion.li>
             <motion.li onClick={() => setCurrentTab(2)}>
-              Graph{" "}
+              Examples{" "}
               {tab === 2 && (
                 <motion.div
                   layoutId="underline"
@@ -57,22 +49,8 @@ export default function Home(props) {
               )}
             </motion.li>
             <motion.li onClick={() => setCurrentTab(3)}>
-              Plot{" "}
+              More info{" "}
               {tab === 3 && (
-                <motion.div
-                  layoutId="underline"
-                  style={{
-                    height: "2px",
-                    width: "100%",
-                    background:
-                      "linear-gradient(90deg, #F78282 0%, #F45B99 100%)",
-                  }}
-                ></motion.div>
-              )}
-            </motion.li>
-            <motion.li onClick={() => setCurrentTab(4)}>
-              Blob{" "}
-              {tab === 4 && (
                 <motion.div
                   layoutId="underline"
                   style={{
@@ -89,25 +67,8 @@ export default function Home(props) {
         {/*                                               */}
         {/* display components if certain state is active */}
         {/*                                               */}
-        <div style={{ display: "flex", height: "800px" }}>
-          {tab == 1 && (
-            <div>
-              {/* <Dots></Dots>
-              <ExpDots></ExpDots>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br> */}
-              <Form></Form>
-            </div>
-          )}
-          {tab == 2 && <Polygon></Polygon>}
-          {tab == 3 && (
-            <TwitterData3
-              color={"linear-gradient(180deg, #BF354F, #D75A75)"}
-            ></TwitterData3>
-          )}
-          {tab == 4 && <Blob></Blob>}
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          {tab == 1 && <Form></Form>}
         </div>
       </main>
     </div>
